ae1c23e2ecdbe1411b5363984ca39bca463dfbe0
bennewhall <bennewhall@me.com>
2022-03-10 13:31:18 -0800

    changed number of jobs for ninja

--- builder.sh
+++ builder.sh
@@ -10,5 +10,5 @@ mkdir build
 cd build
 
 cmake -GNinja -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_TARGETS_TO_BUILD=EVM -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=EVM -DCMAKE_INSTALL_PREFIX=$out ..
-ninja -j8 install
+ninja -j$(( $(nproc) + 1)) install
 
